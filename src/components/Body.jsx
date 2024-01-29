c2950919ed5c17d5418dd7d4ba294d2a533707fb
adityaparab04 <adityaparab04@gmail.com>
2024-01-28 20:05:12 -0500

    top rated and show all restaurants button added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resDataList from "../utils/data";
 import SEARCH from "../utils/constants";
 
 const Body = () => {
+  const [listOfRes, setlistOfRes] = useState(resDataList);
   return (
     <div className="body">
       <div className="search">{SEARCH}</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setlistOfRes(resDataList);
+          }}
+        >
+          Show All
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setlistOfRes(
+              resDataList.filter((res) => {
+                return Number(res.data.avgRating) > 4;
+              })
+            );
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="restaurant-card-container">
-        {resDataList.map((restaurant) => (
+        {listOfRes.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
